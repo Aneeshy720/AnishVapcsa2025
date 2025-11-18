1a514246b89d3a6e38886db80d42a35370265470
Aneeshy720 <103208993+Aneeshy720@users.noreply.github.com>
2025-11-18 03:22:37 +0000

    wrote code for Card.java and Deck.java

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,46 @@
 package cards;
 
 public class Card {
+    private int suit; 
+    private int value; 
+    public static final String[] SUIT = {"♦", "♣", "♥", "♠"}; 
+    public static final String[] VALUE = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
+    public Card(int suit, int value){
+        if(suit<0){
+            this.suit=0; 
+        }
+        else if(suit>SUIT.length-1){
+            this.suit = SUIT.length-1; 
+        }
+        else{
+            this.suit = suit; 
+        }
+
+        if(value<0){
+            this.value = 0; 
+        }
+        else if(value>VALUE.length-1){
+            this.value = VALUE.length-1;
+        }
+        else{
+            this.value = value; 
+        }
+        
+    }
+    public int getValue(){
+        return value;
+    } 
+
+    public String toString(){
+        return SUIT[suit]+VALUE[value]; 
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Card(0, 10)); 
+        System.out.println(new Card(-11, 10));
+        System.out.println(new Card(2, 15));
+        System.out.println(new Card(100, 100));
+    }
 
 }
--- cards/Deck.java
+++ cards/Deck.java
@@ -1,6 +1,92 @@
 package cards;
 
 public class Deck {
+    private int currentCardIndex = 0; 
+    private Card[] playingCards = new Card[Card.SUIT.length * Card.VALUE.length]; // making the size of the array 52 
+    public Deck(){
+        int index = 0; 
+        for(int suit = 0; suit<Card.SUIT.length; suit++){
+            for(int value = 0; value<Card.VALUE.length; value++){
+                playingCards[index] = new Card(suit, value); 
+                index++;                 
+            }
+        }
+    }
+    public Card draw(){
+        if(currentCardIndex<playingCards.length){
+            return playingCards[currentCardIndex++]; 
+        }
+        return null; 
+    }
+
+    public void cut(int num){
+        // if num is out of the boundaries there will not be an amount to cut
+        if(num<1 || num>playingCards.length ){  
+            return;
+        }
+        
+
+        Card[] cutPlayingCards = new Card[Card.SUIT.length * Card.VALUE.length];
+        for(int i = num; i<playingCards.length; i++){
+            cutPlayingCards[i-num] = playingCards[i]; 
+        }
+
+        for(int i = 0; i<num; i++){
+            cutPlayingCards[playingCards.length - num + i] = playingCards[i];
+        }
+        playingCards = cutPlayingCards; 
+    }
+
+    public void print(int numCards){ 
+        if(numCards>playingCards.length){
+            numCards = playingCards.length;
+        }
+        for(int i = 0; i<numCards; i++){
+            System.out.print(playingCards[i]+" "); 
+        }
+        System.out.println(); 
+    }
+
+    public void shuffle(){
+        for(int i=0; i<1000; i++){
+            int firstIndex = (int) (Math.random()*52); 
+            int secondIndex = (int) (Math.random()*52); 
+            if(firstIndex!=secondIndex){
+                Card temp = playingCards[firstIndex]; 
+                playingCards[firstIndex] = playingCards[secondIndex]; 
+                playingCards[secondIndex] = temp; 
+            }
+        }
+    }
+
+    public void resetDraw(){
+        currentCardIndex = 0; 
+    }
+
+    public static void main(String[] args){
+        Deck deck = new Deck();
+        for(int i = 0; i<53; i++){
+            System.out.println(deck.draw()); 
+        }
+        System.out.println("Printing 5 cards:"); 
+        deck.print(5); 
+        System.out.println("Printing -5 cards:"); 
+        deck.print(-5);
+        System.out.println("Printing 100 cards:"); 
+        deck.print(100);
+        System.out.println("Now shuffling ..."); 
+        deck.shuffle();
+        deck.resetDraw(); 
+        deck.print(52); 
+        deck.cut(52); 
+        deck.print(52); 
+        System.out.println("Printing 5 cards:"); 
+        deck.print(5); 
+        System.out.println("Printing -5 cards:"); 
+        deck.print(-5);
+        System.out.println("Printing 100 cards:"); 
+        deck.print(100); 
+    }
 
 
 }
