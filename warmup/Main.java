118db9c030e9011d752f1bdb215e99d7ee60a958
Aneeshy720 <103208993+Aneeshy720@users.noreply.github.com>
2026-01-07 23:11:22 +0000

    completed problem 2 from project euler

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,25 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        // Your code here.
+        //problem 2
+        int[] nums = new int[100]; 
+        nums[0] = 0; 
+        nums[1] = 1; 
+        int sum = 0; 
+        for(int i = 2; i<nums.length; i++){
+            nums[i] = nums[i-1] + nums[i-2]; 
+            if(nums[i]>4000000){
+                break; 
+            }
+            if(nums[i]%2==0){
+                sum = sum+nums[i]; 
+            }
+        }
+        System.out.println("The sum of all the even number in fibonacci up till 4 million is: "+sum); 
+
+        
+        
+
 
     }
 }
